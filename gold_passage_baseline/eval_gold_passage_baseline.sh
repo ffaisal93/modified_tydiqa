4a3a1ce33e589d74d6f48e497e9a22713aca4012
Fahim Faisal <ffaisal@ARGO-1.orc.gmu.edu>
2020-12-30 03:08:49 -0500

    gold sh file updated

--- gold_passage_baseline/eval_gold_passage_baseline.sh
+++ gold_passage_baseline/eval_gold_passage_baseline.sh
@@ -8,16 +8,16 @@ working_dir="$2"
 
 # Evaluation script.
 # https://github.com/allenai/bi-att-flow/blob/master/squad/evaluate-v1.1.py
-EVAL_SCRIPT="$HOME/software/bi-att-flow/squad/evaluate-v1.1.py"
+EVAL_SCRIPT="$HOME/dialect/software/bi-att-flow/squad/evaluate-v1.1.py"
 
 # Path to TyDi QA baseline code (used for splitting predictions into individual
 # languages.
-TYDIQA_REPO_DIR=$PWD/..
+TYDIQA_REPO_DIR=$HOME/dialect/tydiqa
 
 # Path to the TyDi QA Gold Passage (GoldP) task data
 # IMPORTANT: Please report this task name in your results tables as
 # 'TyDiQA-GoldP' to avoid confusing it with the primary tasks.
-TYDIQA_GOLDP_DIR=$HOME/tydiqa_goldp
+TYDIQA_GOLDP_DIR=$HOME/dialect/data/goldp/tydiqa-goldp-v1.1-dev
 TYDIQA_VERSION="1.1"
 
 python3 "${TYDIQA_REPO_DIR}/gold_passage_baseline/split_predictions.py" \
@@ -26,7 +26,7 @@ python3 "${TYDIQA_REPO_DIR}/gold_passage_baseline/split_predictions.py" \
   --lang_output_json_pattern='tydiqa-goldp-dev-predictions-%s.json'
 
 for lang in english arabic bengali finnish indonesian swahili korean russian telugu; do
-  dataset_file="${TYDIQA_GOLDP_DIR}/${TYDIQA_VERSION}/tydiqa-goldp-dev-${TYDIQA_VERSION}-${lang}.json"
+  dataset_file="${TYDIQA_GOLDP_DIR}/tydiqa-goldp-dev-${lang}.json"
   predict_file="${working_dir}/tydiqa-goldp-dev-predictions-${lang}.json"
   echo "Language: ${lang}"
 
--- gold_passage_baseline/run_gold_passage_baseline.sh
+++ gold_passage_baseline/run_gold_passage_baseline.sh
@@ -11,22 +11,23 @@
 set -ueo pipefail  # Halt on all manner of errors.
 set -x  # Display each command as it is executed.
 
-working_dir=$HOME/tydiqa_baseline
+working_dir=$HOME/dialect
+output_dir=/scratch/ffaisal/goldp
 
 # Path to vanilla BERT code.
 # Download with: `git clone https://github.com/google-research/bert`
-BERT_CODE_DIR="${HOME}/software/bert"
+BERT_CODE_DIR="${HOME}/dialect/bert"
 
-TYDIQA_REPO_DIR="${PWD}/.."
+TYDIQA_REPO_DIR="$HOME/dialect/tydiqa"
 
 # Path to Multilingual BERT model (2018-08-23 version with 104 languages)
 # Download from https://storage.googleapis.com/bert_models/2018_11_23/multi_cased_L-12_H-768_A-12.zip
-MBERT_MODEL_DIR="${HOME}/multi_cased_L-12_H-768_A-12"
+MBERT_MODEL_DIR="${HOME}/dialect/software/mbert/multi_cased_L-12_H-768_A-12"
 
 # Path to the TyDi QA Gold Passage (GoldP) task data
 # IMPORTANT: Please report this task name in your results tables as
 # 'TyDiQA-GoldP' to avoid confusing it with the primary tasks.
-TYDIQA_GOLDP_DIR="${HOME}/tydiqa_goldp"
+TYDIQA_GOLDP_DIR="${HOME}/dialect/data/goldp"
 VERSION="v1.1"
 
 python $BERT_CODE_DIR/run_squad.py \
@@ -35,12 +36,12 @@ python $BERT_CODE_DIR/run_squad.py \
   --do_lower_case=False \
   --init_checkpoint="${MBERT_MODEL_DIR}/bert_model.ckpt" \
   --do_train=True \
-  --train_file="${TYDIQA_GOLDP_DIR}/tydiqa-goldp-train-${VERSION}.json" \
+  --train_file="${TYDIQA_GOLDP_DIR}/tydiqa-goldp-${VERSION}-train.json" \
   --do_predict=True \
-  --predict_file="${TYDIQA_GOLDP_DIR}/tydiqa-goldp-dev-${VERSION}.json" \
-  --train_batch_size=12 \
+  --predict_file="${TYDIQA_GOLDP_DIR}/tydiqa-goldp-${VERSION}-dev.json" \
+  --train_batch_size=5 \
   --learning_rate=3e-5 \
   --num_train_epochs=2.0 \
   --max_seq_length=384 \
   --doc_stride=128 \
-  --output_dir="${working_dir}"
+  --output_dir="${output_dir}"
